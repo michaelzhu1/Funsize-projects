013e8b27b57253ee5834696b6779f76de46e07af
Michael <mzhu105@gmail.com>
2017-11-01 21:36:56 -0700

    Basic game functions

--- ghost/game.rb
+++ ghost/game.rb
@@ -4,43 +4,78 @@ require 'byebug'
 
 class Game
   ALPHABET = Set.new('a'..'z')
-  def initialize(player1, player2)
+  def initialize(*players)
     words = IO.readlines('dictionary.txt').map(&:chomp)
     @dict = Set.new(words)
-    @player1 = player1
-    @player2 = player2
+    @players = players
     @fragment = ''
+    @losses = Hash.new { |hash, player| hash[player] = 0 }
   end
 
   def run
-    play_round until game_over?
+    system("clear")
+    print "Welcome to GHOST game"
 
+    play_round until game_over?
+    puts "#{self.current_player.name} lost"
   end
 
   def play_round
-
+    until round_over?
+      take_turn
+    end
+    update_score
   end
 
   def current_player
-    @player1
+    @players[0]
   end
 
   def previous_player
-    @player2
+    @players[-1]
   end
 
   def next_player!
-    self.current_player, self.previous_player = self.previous_player, self.current_player
+    @players.push(@players.shift)
   end
 
-  def take_turn(player)
+  def take_turn
+    new_letter = get_letter()
+    if valid_play?(new_letter)
+      @fragment += new_letter
 
+    else
+      puts "Invalid letter, please try again"
+      take_turn
+    end
   end
 
   def valid_play?(string)
     return false unless ALPHABET.include?(string)
     new_fragment = @fragment + string
     @dict.any? { |word| word.start_with?(new_fragment) }
+  end
+
+  def get_letter()
+    puts "The current word is #{@fragment}" if @fragment
+    puts "Please enter a letter"
+    gets.chomp
+  end
+
+  def game_over?
+    @losses.values.any? { |losses| losses > 2 }
+  end
+
+  def is_word?(word)
+    @dict.include?(word)
+  end
+
+  def round_over?
+    is_word?(@fragment)
+  end
+
+  def update_score
+    @losses[self.current_player] += 1
   end
 end
 
--- ghost/player.rb
+++ ghost/player.rb
@@ -1,10 +1,11 @@
 class Player
+  attr_reader :name 
   def initialize(name)
     @name = name
   end
 
   def guess
-    
+
   end
 
   def alert_invalid_guess
