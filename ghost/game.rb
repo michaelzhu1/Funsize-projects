5f5bb34e2cb763dcdc924ba607c332f391112776
Michael <mzhu105@gmail.com>
2017-11-02 13:13:19 -0700

    Complete game

--- ghost/game.rb
+++ ghost/game.rb
@@ -1,89 +1,143 @@
-require_relative 'player.rb'
-require 'Set'
-require 'byebug'
+require "set"
+require_relative "player"
 
-class Game
-  ALPHABET = Set.new('a'..'z')
+class GhostGame
+  ALPHABET = Set.new("a".."z")
+  MAX_LOSS_COUNT = 5
+
   def initialize(*players)
-    words = IO.readlines('dictionary.txt').map(&:chomp)
-    @dict = Set.new(words)
+    words = File.readlines("dictionary.txt").map(&:chomp)
+    @dictionary = Set.new(words)
     @players = players
-    @fragment = ''
-    @losses = Hash.new { |hash, player| hash[player] = 0 }
+    @losses = Hash.new { |losses, player| losses[player] = 0 }
   end
 
   def run
-    system("clear")
-    print "Welcome to GHOST game"
-
     play_round until game_over?
-    puts "#{self.current_player.name} lost"
+    puts "#{winner} wins!"
+  end
+
+  private
+  attr_reader :fragment, :dictionary, :losses, :players
+
+  def game_over?
+    remaining_players == 1
   end
 
   def play_round
+    @fragment = ""
+    welcome
+
     until round_over?
       take_turn
+      next_player!
     end
-    update_score
+
+    update_standings
   end
 
-  def current_player
-    @players[0]
+  # helper methods
+  def add_letter(letter)
+    fragment << letter
   end
 
-  def previous_player
-    @players[-1]
+  def valid_play?(letter)
+    return false unless ALPHABET.include?(letter)
+
+    potential_fragment = fragment + letter
+    dictionary.any? { |word| word.start_with?(potential_fragment) }
   end
 
-  def next_player!
-    @players.push(@players.shift)
-  end
-
-  def take_turn
-    new_letter = get_letter
-    if valid_play?(new_letter)
-      @fragment += new_letter
-
-    else
-      puts "Invalid letter, please try again"
-      take_turn
-    end
-  end
-
-  def valid_play?(string)
-    return false unless ALPHABET.include?(string)
-    new_fragment = @fragment + string
-    @dict.any? { |word| word.start_with?(new_fragment) }
-  end
-
-  def get_letter()
-    puts "The current word is #{@fragment}" if @fragment
-    puts "Please enter a letter"
-    gets.chomp
-  end
-
-  def game_over?
-    @losses.values.any? { |losses| losses > 2 }
-    byebug
-  end
-
-  def is_word?(word)
-    @dict.include?(word)
+  def is_word?(fragment)
+    dictionary.include?(fragment)
   end
 
   def round_over?
-    is_word?(@fragment)
+    is_word?(fragment)
   end
 
-  def update_score
-    @losses[self.current_player] += 1
+  def current_player
+    players.first
+  end
+
+  def previous_player
+    players.last
+  end
+
+  def next_player!
+    players.rotate!
+    # keep rotating until we find a player who hasn't been eliminated
+    players.rotate! until losses[current_player] < MAX_LOSS_COUNT
+  end
+
+  def remaining_players
+    losses.count { |_, v| v < MAX_LOSS_COUNT }
+  end
+
+  def winner
+    (player, _) = losses.find { |_, losses| losses < MAX_LOSS_COUNT }
+    player
+  end
+
+  def record(player)
+    count = losses[player]
+    "GHOST".slice(0, count)
+  end
+
+  # UI methods (display game status and prompts to players)
+  def welcome
+    system("clear")
+    puts "Let's play a round of Ghost!"
+    display_standings
+  end
+
+  def take_turn
+    system("clear")
+    puts "It's #{current_player}'s turn!"
+    letter = nil
+
+    until letter
+      letter = current_player.guess(fragment)
+
+      unless valid_play?(letter)
+        current_player.alert_invalid_move(letter)
+        letter = nil
+      end
+    end
+
+    add_letter(letter)
+    puts "#{current_player} added the letter '#{letter}' to the fragment."
+  end
+
+  def display_standings
+    system("clear")
+    puts "Current standings:"
+
+    players.each do |player|
+      puts "#{player}: #{record(player)}"
+    end
+
+    sleep(2)
+  end
+
+  def update_standings
+    system("clear")
+    puts "#{previous_player} spelled #{fragment}."
+    puts "#{previous_player} gets a letter!"
+
+    losses[previous_player] += 1
+
+    if losses[previous_player] == MAX_LOSS_COUNT
+      puts "#{previous_player} has been eliminated!"
+    end
+
+    display_standings
+
+    sleep(2)
   end
 end
 
-
 if __FILE__ == $PROGRAM_NAME
-  mike = Player.new("mike")
-  tom = Player.new("tom")
-  game = Game.new(mike,tom)
+  game = GhostGame.new(Player.new("Mike"), Player.new("Tom"))
   game.run
 end
