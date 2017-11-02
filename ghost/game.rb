f03c30e2a22c885b965bf70deda88aff6a5a0241
Michael <mzhu105@gmail.com>
2017-11-01 20:41:40 -0700

    Add valid_play? method

--- ghost/game.rb
+++ ghost/game.rb
@@ -1,11 +1,20 @@
 require_relative 'player.rb'
+require 'Set'
+require 'byebug'
 
 class Game
+  ALPHABET = Set.new('a'..'z')
   def initialize(player1, player2)
+    words = IO.readlines('dictionary.txt').map(&:chomp)
+    @dict = Set.new(words)
     @player1 = player1
     @player2 = player2
-    @fragment = nil
-    @dict = nil
+    @fragment = ''
+  end
+
+  def run
+    play_round until game_over?
+
   end
 
   def play_round
@@ -25,10 +34,20 @@ class Game
   end
 
   def take_turn(player)
-    
+
   end
 
   def valid_play?(string)
-
+    return false unless ALPHABET.include?(string)
+    new_fragment = @fragment + string
+    @dict.any? { |word| word.start_with?(new_fragment) }
   end
+end
+
+
+if __FILE__ == $PROGRAM_NAME
+  mike = Player.new("mike")
+  tom = Player.new("tom")
+  game = Game.new(mike,tom)
+  game.run
 end
