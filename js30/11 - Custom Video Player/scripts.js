adeb26004b637e55b85c5ac267aaf34668c0abd8
Michael <mzhu105@gmail.com>
2017-11-17 12:03:50 -0800

    Grab all the elements

---
+++ js30/11 - Custom Video Player/scripts.js
@@ -0,0 +1,15 @@
+// Get our elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+
+const ranges = player.querySelectorAll('.player__slider');
+const skips = player.querySelectorAll('[data-skip]');
+
+
+
+// Build our functions
+
+// Hook up the event listeners
