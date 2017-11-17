43aa64f487a75a190d587729721b440778f3cfe0
Michael <mzhu105@gmail.com>
2017-11-17 12:15:13 -0800

    Add eventlisters for toggleplay

--- js30/11 - Custom Video Player/scripts.js
+++ js30/11 - Custom Video Player/scripts.js
@@ -4,12 +4,21 @@ const video = player.querySelector('.viewer');
 
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
-
+const playButton = player.querySelector('.toggle');
 const ranges = player.querySelectorAll('.player__slider');
 const skips = player.querySelectorAll('[data-skip]');
 
 
-
 // Build our functions
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
 
 // Hook up the event listeners
+video.addEventListener('click', togglePlay);
+playButton.addEventListener('click', togglePlay);
