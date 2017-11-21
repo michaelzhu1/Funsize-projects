32bdce5a15ab217f2a29c9c51d34a391dc6db8d7
Michael <mzhu105@gmail.com>
2017-11-20 18:34:28 -0800

    Add AddProject component

---
+++ project-manager/src/Components/AddProject.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class AddProject extends Component {
+  render() {
+    return (
+      <div className="projects">
+      </div>
+    );
+  }
+}
+
+export default AddProject;
