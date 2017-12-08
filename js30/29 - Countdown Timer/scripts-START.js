96f4f1ec76e637a04a7e3c9911bd6bc5bce43170
Michael <mzhu105@gmail.com>
2017-12-08 14:46:41 -0800

    Display the end time when the timer runs out

--- js30/29 - Countdown Timer/scripts-START.js
+++ js30/29 - Countdown Timer/scripts-START.js
@@ -2,6 +2,7 @@ function timer(seconds) {
   const now = Date.now();
   displayTimeLeft(seconds);
   const then = now + seconds * 1000;
+  displayEndTime(then);
   const countdown = setInterval(() => {
     const timeLeft = Math.round((then - Date.now()) / 1000);
     if (timeLeft < 0) {
@@ -18,7 +19,18 @@ function displayTimeLeft(seconds) {
   const remainderSeconds = seconds % 60;
   const display = `${minutes < 10 ? '0' : ''}${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
   timerDisplay.textContent = display;
+  // Change the title of page to the timer itself
+  document.title = display;
 }
 
+function displayEndTime(end) {
+  const time = new Date(end);
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const hours = time.getHours();
+
+  endTimeDisplay.textContent = `Be back at ${hours > 12 ? hours - 12 : hours}:${minutes < 10 ? '0' : ''}${minutes}:${seconds}`;
+}
 
 const timerDisplay = document.querySelector('.display__time-left');
+const endTimeDisplay = document.querySelector('.display__end-time');
