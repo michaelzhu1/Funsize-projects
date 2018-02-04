015c043374d1ce398677ca026c887a9dd9eb524c
Michael <mzhu105@gmail.com>
2018-02-04 13:52:07 -0800

    Clock component display in root

---
+++ react-widgets/frontend/clock.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Clock extends React.Component {
+    render() {
+      return(
+        <div>
+          <h1>Clock</h1>
+        </div>
+      );
+    }
+}
+
+
+
+export default Clock;
--- react-widgets/frontend/widgets.jsx
+++ react-widgets/frontend/widgets.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Clock from './clock';
+
 class Root extends React.Component {
   render() {
     return (
-      <div>what's up</div>
+      <div>
+        <Clock />
+      </div>
     );
   }
 }
