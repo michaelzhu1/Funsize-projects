627c1fe0fe4190efd0ad4d1b221b2035c09bffbb
Michael <mzhu105@gmail.com>
2018-02-07 00:10:16 -0800

    Set up clock ticking

--- react-widgets/frontend/clock.jsx
+++ react-widgets/frontend/clock.jsx
@@ -4,15 +4,34 @@ import React from 'react';
 class Clock extends React.Component {
   constructor(prop) {
     super(prop);
-
+    this.state = {
+      time: new Date()
+    };
   }
-    render() {
-      return(
-        <div>
-          <h1>Clock</h1>
-        </div>
-      );
-    }
+
+  componentDidMount() {
+    this.tick();
+  }
+
+  tick() {
+    this.interval = setInterval(() => {
+      this.setState({time: new Date()});
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const time = this.state.time;
+    return(
+      <div>
+        <h1>Clock</h1>
+        {time.getHours()} : {time.getMinutes()} : {time.getSeconds()}
+      </div>
+    );
+  }
 }
 
 
