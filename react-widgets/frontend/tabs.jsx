92f7cbcb23fe2c351e42b1c81112269690af9283
Michael <mzhu105@gmail.com>
2018-02-07 22:26:27 -0800

    Create Header component

--- react-widgets/frontend/tabs.jsx
+++ react-widgets/frontend/tabs.jsx
@@ -1,15 +1,51 @@
 import React from 'react';
 
+const Header = (props) => {
+  let headers = props.panes.map((pane, index) => {
+    let title = pane.title;
+    let style = '';
+    if (index === props.currentTab) {
+      style = 'active';
+    }
+
+    return (
+      <li
+        key={index}
+        className={style}
+        onClick={props.onTabChosen.bind(null, index)}>
+        {title}{' '}
+      </li>
+    );
+  });
+  return (
+    <ul>
+      {headers}
+    </ul>
+  );
+};
+
+
 
 class Tabs extends React.Component{
   constructor(props) {
     super(props);
+    this.state = {
+      currentTab: 0
+    };
+  }
+
+  selectTab(num) {
+    this.setState({ currentTab: num });
   }
 
   render() {
     return(
       <div className="tabs">
-        tabs
+        <h1>Tabs</h1>
+        <Header
+          currentTab={this.state.currentTab}
+          onTabChosen={this.selectTab}
+          panes={this.props.tabs}/>
       </div>
     );
   }
