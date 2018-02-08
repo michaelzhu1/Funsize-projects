b0b0753aabf26633e940b9bcf20b048b9c795b2b
Michael <mzhu105@gmail.com>
2018-02-07 16:44:42 -0800

    Create tab informations in root to pass down to tabs component

---
+++ react-widgets/frontend/tabs.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class Tabs extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div className="tabs">
+        tabs
+      </div>
+    );
+  }
+}
+
+export default Tabs;
--- react-widgets/frontend/widgets.jsx
+++ react-widgets/frontend/widgets.jsx
@@ -2,12 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Clock from './clock';
+import Tabs from './tabs';
+
+const tabs = [
+  {title: 'First tab', content: 'Nothing too interesting'},
+  {title: 'Second tab', content: 'You should go back to first tab'},
+  {title: 'Third tab', content: 'This is the last tab for now'}
+];
+
+
+
+
 
 class Root extends React.Component {
   render() {
     return (
       <div>
         <Clock />
+        <Tabs tabs={tabs}/>
       </div>
     );
   }
