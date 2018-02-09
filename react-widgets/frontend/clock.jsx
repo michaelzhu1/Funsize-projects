bf3318750821efdabf21c229b01765e4377b821b
Michael <mzhu105@gmail.com>
2018-02-08 17:47:15 -0800

    Add clock display

--- react-widgets/frontend/clock.jsx
+++ react-widgets/frontend/clock.jsx
@@ -7,6 +7,7 @@ class Clock extends React.Component {
     this.state = {
       time: new Date()
     };
+    this.setDate = this.setDate.bind(this);
   }
 
   componentDidMount() {
@@ -16,11 +17,27 @@ class Clock extends React.Component {
   tick() {
     this.interval = setInterval(() => {
       this.setState({time: new Date()});
+      this.setDate();
     }, 1000);
   }
 
   componentWillUnmount() {
     clearInterval(this.interval);
+  }
+
+  setDate() {
+    const seconds = this.state.time.getSeconds();
+    const minutes = this.state.time.getMinutes();
+    const hours = this.state.time.getHours();
+    const secondHand = document.querySelector(".second-hand");
+    const minuteHand = document.querySelector(".min-hand");
+    const hourHand = document.querySelector(".hour-hand");
+    let secondsDegrees = seconds / 60 * 360 + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    let minutesDegrees = minutes / 60 * 360 + 90;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    let hoursDegrees = hours / 12 * 360 + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
   }
 
   render() {
@@ -33,23 +50,30 @@ class Clock extends React.Component {
     return(
       <div>
         <div className="clock">
-          <h1>Clock</h1>
-          <p>
-            <span>
-              Time:
-            </span>
-            <span>
-              {hours} : {minutes} : {seconds}
-            </span>
-          </p>
-          <p>
-            <span>
-              Date:
-            </span>
-            <span>
-              {this.state.time.toDateString()}
-            </span>
-          </p>
+          <div className="clock-face">
+            <div className="hand hour-hand"></div>
+            <div className="hand min-hand"></div>
+            <div className="hand second-hand"></div>
+          </div>
+          <div className="">
+            <h1>Clock</h1>
+            <p>
+              <span>
+                Time:
+              </span>
+              <span>
+                {hours} : {minutes} : {seconds}
+              </span>
+            </p>
+            <p>
+              <span>
+                Date:
+              </span>
+              <span>
+                {this.state.time.toDateString()}
+              </span>
+            </p>
+          </div>
         </div>
       </div>
     );
