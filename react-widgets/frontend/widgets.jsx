8cea85f5ad73bf35da80cd495d16da2900813295
Michael <mzhu105@gmail.com>
2018-02-08 16:18:55 -0800

    Add more style to clock

--- react-widgets/frontend/widgets.jsx
+++ react-widgets/frontend/widgets.jsx
@@ -4,15 +4,13 @@ import ReactDOM from 'react-dom';
 import Clock from './clock';
 import Tabs from './tabs';
 
+
+// tabs information that gets passed down to Tabs component
 const tabs = [
   {title: 'First tab', content: 'Nothing too interesting'},
   {title: 'Second tab', content: 'You should go back to first tab'},
   {title: 'Third tab', content: 'This is the last tab for now'}
 ];
-
-
-
-
 
 class Root extends React.Component {
   render() {
@@ -25,6 +23,9 @@ class Root extends React.Component {
   }
 }
 
+
+
+// Render the Root component that includes all widgets
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<Root/>, document.getElementById('main'));
 });
